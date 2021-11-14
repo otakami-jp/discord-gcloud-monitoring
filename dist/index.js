1662e0749953d2d62951b3bcfa17a8d362de31d0
Rayane <guemmoudrayane03@gmail.com>
2021-11-15 00:10:54 +0100

    fix

--- dist/index.js
+++ dist/index.js
@@ -25,15 +25,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http = __importStar(require("http"));
 const axios_1 = __importDefault(require("axios"));
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
-const authenticate = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD).toString('base64');
+const authenticate = Buffer.from(process.env.AUTH_USERNAME + ':' + process.env.AUTH_PASSWORD).toString('base64');
 function application(request, response) {
-    const headers = request.headers.authorization;
+    const authorization = request.headers.authorization;
     let data = '';
     function handlerData(chunk) {
-        data += chunk;
+        console.log(chunk);
+        data += Buffer.from(chunk).toString('utf-8');
     }
-    request.socket.on('data', handlerData);
-    if (typeof headers === 'undefined') {
+    ;
+    if (typeof authorization === 'undefined') {
         request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
@@ -43,8 +44,9 @@ function application(request, response) {
         return;
     }
     ;
-    const [, auth] = headers.split(' ');
+    const [, auth] = authorization.split(' ');
     if (auth !== authenticate) {
+        console.log('[SERVER] authentificate failed : %s (%s)', Buffer.from(auth).toString('utf-8'), auth);
         request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
@@ -56,69 +58,98 @@ function application(request, response) {
     ;
     request.once('end', () => {
         let monitoring = JSON.parse(data);
-        axios_1.default.post(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
-            content: monitoring.incident.documentation.content,
-            embeds: [
-                {
-                    title: 'Incident report (' + monitoring.incident.resource_name + ')',
-                    type: 'rich',
-                    description: monitoring.incident.summary,
-                    url: monitoring.incident.url,
-                    color: 0xE12121,
-                    footer: {
-                        text: 'Project ID : ' + monitoring.incident.resource.labels.project_id
-                    },
-                    author: {
-                        name: 'Resource : ' + monitoring.incident.resource_display_name
-                    },
-                    fields: [
-                        {
-                            name: 'Monitoring version',
-                            value: 'v' + monitoring.version,
-                            inline: true
+        try {
+            axios_1.default.post(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
+                content: monitoring.incident.documentation.content,
+                embeds: [
+                    {
+                        title: 'Incident report (' + monitoring.incident.resource_name + ')',
+                        type: 'rich',
+                        description: monitoring.incident.summary,
+                        url: monitoring.incident.url,
+                        color: 0xE12121,
+                        footer: {
+                            text: 'Project ID : ' + monitoring.incident.resource.labels.project_id
                         },
-                        {
-                            name: 'Scoping project id',
-                            value: monitoring.incident.scoping_project_id,
-                            inline: true
+                        author: {
+                            name: 'Resource : ' + monitoring.incident.resource_display_name
                         },
-                        {
-                            name: 'Incident timing',
-                            value: (monitoring.incident.ended_at - monitoring.incident.started_at) / 60000 + ' min',
-                            inline: true
-                        },
-                        {
-                            name: 'Incident incident',
-                            value: monitoring.incident.state,
-                            inline: true
-                        },
-                        {
-                            name: 'Resource type',
-                            value: monitoring.incident.resource_type_display_name,
-                            inline: true
-                        },
-                        {
-                            name: 'Resource name',
-                            value: monitoring.incident.resource_name,
-                            inline: true
-                        }
-                    ]
-                }
-            ]
-        })
-            .then(() => {
-            console.log('[REQUEST] Success');
-        })
-            .catch((error) => {
-            console.log('[REQUEST] Error');
-            console.error(error);
-        })
-            .finally(() => {
-            request.removeListener('data', handlerData);
-            response.writeHead(204);
-            response.end();
-        });
+                        fields: [
+                            {
+                                name: 'Monitoring version',
+                                value: 'v' + monitoring.version,
+                                inline: true
+                            },
+                            {
+                                name: 'Scoping project id',
+                                value: monitoring.incident.scoping_project_id,
+                                inline: true
+                            },
+                            {
+                                name: 'Incident timing',
+                                value: (monitoring.incident.ended_at - monitoring.incident.started_at) / 60000 + ' min',
+                                inline: true
+                            },
+                            {
+                                name: 'Incident incident',
+                                value: monitoring.incident.state,
+                                inline: true
+                            },
+                            {
+                                name: 'Resource type',
+                                value: monitoring.incident.resource_type_display_name,
+                                inline: true
+                            },
+                            {
+                                name: 'Resource name',
+                                value: monitoring.incident.resource_name,
+                                inline: true
+                            }
+                        ]
+                    }
+                ]
+            })
+                .then(() => {
+                console.log('[REQUEST] Success webhook');
+                response.writeHead(204);
+            })
+                .catch((error) => {
+                console.log('[REQUEST] Error webhook');
+                console.error(error);
+                response.writeHead(400);
+            })
+                .finally(() => {
+                request.removeListener('data', handlerData);
+                console.log('[REQUEST] Close connection');
+                response.end();
+            });
+        }
+        catch (err) {
+            console.log('[SERVER] parsing error');
+            console.error(err);
+            axios_1.default
+                .post(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
+                content: 'Error parsing data when received data from GCP monitoring',
+                color: 0xE12121
+            })
+                .then(() => {
+                console.log('[REQUEST] Success webhook');
+                response.writeHead(204);
+            })
+                .catch((error) => {
+                console.log('[REQUEST] Error webhook');
+                console.error(error);
+                response.writeHead(400);
+            })
+                .finally(() => {
+                request.removeListener('data', handlerData);
+                console.log('[REQUEST] Close connection');
+                response.end();
+            });
+        }
+        ;
     });
+    request.on('data', handlerData);
 }
 ;
 const server = http.createServer(application);
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,18 @@ import * as net from 'net';
 import axios from 'axios';
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8080;
-const authenticate: string = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD).toString('base64');
+const authenticate: string = Buffer.from(process.env.AUTH_USERNAME + ':' + process.env.AUTH_PASSWORD).toString('base64');
 
 function application(request: http.IncomingMessage, response: http.ServerResponse): void {
-    const headers: string | undefined = request.headers.authorization;
-    let data = '';
+    const authorization: string | undefined = request.headers.authorization;
+    let data: string = '';
 
     function handlerData(chunk: string): void {
-        data += chunk;
-    }
+        console.log(chunk);
+        data += Buffer.from(chunk).toString('utf-8');
+    };
 
-    request.socket.on('data', handlerData);
-
-    if (typeof headers === 'undefined') {
+    if (typeof authorization === 'undefined') {
         request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
@@ -25,9 +24,10 @@ function application(request: http.IncomingMessage, response: http.ServerRespons
         return;
     };
 
-    const [, auth] = (<string>headers).split(' ');
+    const [, auth] = (<string>authorization).split(' ');
 
     if (auth !== authenticate) {
+        console.log('[SERVER] authentificate failed : %s (%s)', Buffer.from(auth).toString('utf-8'), auth);
         request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
@@ -39,73 +39,105 @@ function application(request: http.IncomingMessage, response: http.ServerRespons
 
     request.once('end', () => {
         let monitoring: {[key: string]: any} = JSON.parse(data);
-        axios.post(
-            `https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`,
-            {
-                content: monitoring.incident.documentation.content,
-                embeds: [
+        try {
+            axios.post(
+                `https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`,
+                {
+                    content: monitoring.incident.documentation.content,
+                    embeds: [
+                        {
+                            title: 'Incident report (' + monitoring.incident.resource_name + ')',
+                            type: 'rich',
+                            description: monitoring.incident.summary,
+                            url: monitoring.incident.url,
+                            color: 0xE12121,
+                            footer: {
+                                text: 'Project ID : ' + monitoring.incident.resource.labels.project_id
+                            },
+                            author: {
+                                name: 'Resource : ' + monitoring.incident.resource_display_name
+                            },
+                            fields: [
+                                {
+                                    name: 'Monitoring version',
+                                    value: 'v' + monitoring.version,
+                                    inline: true
+                                },
+                                {
+                                    name: 'Scoping project id',
+                                    value: monitoring.incident.scoping_project_id,
+                                    inline: true
+                                },
+                                {
+                                    name: 'Incident timing',
+                                    value: (monitoring.incident.ended_at - monitoring.incident.started_at) / 60000 + ' min',
+                                    inline: true
+                                },
+                                {
+                                    name: 'Incident incident',
+                                    value: monitoring.incident.state,
+                                    inline: true
+                                },
+                                {
+                                    name: 'Resource type',
+                                    value: monitoring.incident.resource_type_display_name,
+                                    inline: true
+                                },
+                                {
+                                    name: 'Resource name',
+                                    value: monitoring.incident.resource_name,
+                                    inline: true
+                                }
+                            ]
+                        }
+                    ]
+                }
+            )
+                .then(() => {
+                    console.log('[REQUEST] Success webhook');
+                    response.writeHead(204);
+                })
+                .catch((error) => {
+                    console.log('[REQUEST] Error webhook');
+                    console.error(error);
+                    response.writeHead(400);
+                })
+                .finally(() => {
+                    request.removeListener('data', handlerData);
+                    console.log('[REQUEST] Close connection');
+    
+                    response.end();
+                });
+        } catch (err) {
+            console.log('[SERVER] parsing error');
+            console.error(err);
+            axios
+                .post(
+                    `https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`,
                     {
-                        title: 'Incident report (' + monitoring.incident.resource_name + ')',
-                        type: 'rich',
-                        description: monitoring.incident.summary,
-                        url: monitoring.incident.url,
-                        color: 0xE12121,
-                        footer: {
-                            text: 'Project ID : ' + monitoring.incident.resource.labels.project_id
-                        },
-                        author: {
-                            name: 'Resource : ' + monitoring.incident.resource_display_name
-                        },
-                        fields: [
-                            {
-                                name: 'Monitoring version',
-                                value: 'v' + monitoring.version,
-                                inline: true
-                            },
-                            {
-                                name: 'Scoping project id',
-                                value: monitoring.incident.scoping_project_id,
-                                inline: true
-                            },
-                            {
-                                name: 'Incident timing',
-                                value: (monitoring.incident.ended_at - monitoring.incident.started_at) / 60000 + ' min',
-                                inline: true
-                            },
-                            {
-                                name: 'Incident incident',
-                                value: monitoring.incident.state,
-                                inline: true
-                            },
-                            {
-                                name: 'Resource type',
-                                value: monitoring.incident.resource_type_display_name,
-                                inline: true
-                            },
-                            {
-                                name: 'Resource name',
-                                value: monitoring.incident.resource_name,
-                                inline: true
-                            }
-                        ]
+                        content: 'Error parsing data when received data from GCP monitoring',
+                        color: 0xE12121
                     }
-                ]
-            }
-        )
-            .then(() => {
-                console.log('[REQUEST] Success');
-            })
-            .catch((error) => {
-                console.log('[REQUEST] Error');
-                console.error(error);
-            })
-            .finally(() => {
-                request.removeListener('data', handlerData);
-
-                response.writeHead(204);
-                response.end();
-            });
+                )
+                .then(() => {
+                    console.log('[REQUEST] Success webhook');
+                    response.writeHead(204);
+                })
+                .catch((error) => {
+                    console.log('[REQUEST] Error webhook');
+                    console.error(error);
+                    response.writeHead(400);
+                })
+                .finally(() => {
+                    request.removeListener('data', handlerData);
+                    console.log('[REQUEST] Close connection');
+    
+                    response.end();
+                });
+        };
     });
+
+    request.on('data', handlerData);
 };
 
 const server: http.Server = http.createServer(application);
