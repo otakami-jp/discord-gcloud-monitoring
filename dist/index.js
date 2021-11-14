e2a7f523f535eca4312d3e5e148819cd391fad3b
Rayane <guemmoudrayane03@gmail.com>
2021-11-14 23:29:33 +0100

    new build version

--- dist/index.js
+++ dist/index.js
@@ -24,12 +24,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = __importStar(require("http"));
 const axios_1 = __importDefault(require("axios"));
-const test_json_1 = __importDefault(require("./test.json"));
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const authenticate = Buffer.from(process.env.USERNAME + ':' + process.env.PASSWORD).toString('base64');
 function application(request, response) {
     const headers = request.headers.authorization;
+    let data = '';
+    function handlerData(chunk) {
+        data += chunk;
+    }
+    request.socket.on('data', handlerData);
     if (typeof headers === 'undefined') {
+        request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
             'WWW-Authenticate': 'Basic realm="Discord Monitoring"'
@@ -40,6 +45,7 @@ function application(request, response) {
     ;
     const [, auth] = headers.split(' ');
     if (auth !== authenticate) {
+        request.removeListener('data', handlerData);
         response.writeHead(401, {
             'Content-Type': 'text/plain',
             'WWW-Authenticate': 'Basic realm="Discord Monitoring"'
@@ -48,62 +54,70 @@ function application(request, response) {
         return;
     }
     ;
-    axios_1.default.post(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
-        content: test_json_1.default.incident.documentation.content,
-        embeds: [
-            {
-                title: '[TEST] incident report (' + test_json_1.default.incident.resource_name + ')',
-                type: 'rich',
-                description: test_json_1.default.incident.summary,
-                url: test_json_1.default.incident.url,
-                color: 0xE12121,
-                footer: {
-                    text: 'Project ID : ' + test_json_1.default.incident.resource.labels.project_id
-                },
-                author: {
-                    name: 'Resource : ' + test_json_1.default.incident.resource_display_name
-                },
-                fields: [
-                    {
-                        name: 'Monitoring version',
-                        value: 'v' + test_json_1.default.version,
-                        inline: true
+    request.once('end', () => {
+        let monitoring = JSON.parse(data);
+        axios_1.default.post(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
+            content: monitoring.incident.documentation.content,
+            embeds: [
+                {
+                    title: 'Incident report (' + monitoring.incident.resource_name + ')',
+                    type: 'rich',
+                    description: monitoring.incident.summary,
+                    url: monitoring.incident.url,
+                    color: 0xE12121,
+                    footer: {
+                        text: 'Project ID : ' + monitoring.incident.resource.labels.project_id
                     },
-                    {
-                        name: 'Scoping project id',
-                        value: test_json_1.default.incident.scoping_project_id,
-                        inline: true
+                    author: {
+                        name: 'Resource : ' + monitoring.incident.resource_display_name
                     },
-                    {
-                        name: 'Incident timing',
-                        value: (test_json_1.default.incident.ended_at - test_json_1.default.incident.started_at) / 60000 + ' min',
-                        inline: true
-                    },
-                    {
-                        name: 'Incident incident',
-                        value: test_json_1.default.incident.state,
-                        inline: true
-                    },
-                    {
-                        name: 'Resource type',
-                        value: test_json_1.default.incident.resource_type_display_name,
-                        inline: true
-                    },
-                    {
-                        name: 'Resource name',
-                        value: test_json_1.default.incident.resource_name,
-                        inline: true
-                    }
-                ]
-            }
-        ]
-    })
-        .then(() => {
-        console.log('[REQUEST] Success');
-    })
-        .catch((error) => {
-        console.log('[REQUEST] Error');
-        console.error(error);
+                    fields: [
+                        {
+                            name: 'Monitoring version',
+                            value: 'v' + monitoring.version,
+                            inline: true
+                        },
+                        {
+                            name: 'Scoping project id',
+                            value: monitoring.incident.scoping_project_id,
+                            inline: true
+                        },
+                        {
+                            name: 'Incident timing',
+                            value: (monitoring.incident.ended_at - monitoring.incident.started_at) / 60000 + ' min',
+                            inline: true
+                        },
+                        {
+                            name: 'Incident incident',
+                            value: monitoring.incident.state,
+                            inline: true
+                        },
+                        {
+                            name: 'Resource type',
+                            value: monitoring.incident.resource_type_display_name,
+                            inline: true
+                        },
+                        {
+                            name: 'Resource name',
+                            value: monitoring.incident.resource_name,
+                            inline: true
+                        }
+                    ]
+                }
+            ]
+        })
+            .then(() => {
+            console.log('[REQUEST] Success');
+        })
+            .catch((error) => {
+            console.log('[REQUEST] Error');
+            console.error(error);
+        })
+            .finally(() => {
+            request.removeListener('data', handlerData);
+            response.writeHead(204);
+            response.end();
+        });
     });
 }
 ;
